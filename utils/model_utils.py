e34718720685ed38987d712ded4b5a816cfa4496
Ken <ken.xiaoliang@gmail.com>
2024-05-25 11:20:08 +0100

    added gpt2 related stuff

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -12,17 +12,49 @@ def load_model_and_tokenizer(model_fpath, tokenizer_only=False):
     load_in_8bit = False
     torch_dtype = torch.float16
 
-    # Load pretrained model
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_fpath,
-        load_in_8bit=load_in_8bit,
-        device_map='auto',
-        trust_remote_code=True,
-        torch_dtype=torch_dtype
-    )
 
-    tokenizer = transformers.AutoTokenizer.from_pretrained(
-        model_fpath,
-    )
+    # Load model trained from scratch from local checkpoint
+    if model_fpath in [
+            "gpt2_scratch",
+            "finetune_gpt2",
+            "gpt2_scratch_neuro_tokenizer"
+        ]:
+        model_fpath = f"/home/ken/projects/matching_experts/model_training/exp/{model_fpath}/checkpoint.4"
+        print("Loading GPT2 model from", model_fpath)
+        model = transformers.GPT2LMHeadModel.from_pretrained(
+            model_fpath,
+            load_in_8bit=load_in_8bit,
+            device_map='auto',
+            trust_remote_code=True,
+            torch_dtype=torch_dtype
+        )
+
+        tokenizer = transformers.GPT2Tokenizer.from_pretrained(
+            model_fpath,    
+        )
+    
+    # Load model untrained (config only)
+    elif model_fpath == "gpt2_init":
+        print("Loading GPT2 model untrained")
+        from transformers import AutoConfig, AutoModelForCausalLM
+        model_config = AutoConfig.from_pretrained("gpt2")
+        model = AutoModelForCausalLM.from_config(model_config).to('cuda')
+        tokenizer = transformers.GPT2Tokenizer.from_pretrained(
+            "gpt2",
+        )
+
+    # Load pretrained model
+    else:
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_fpath,
+            load_in_8bit=load_in_8bit,
+            device_map='auto',
+            trust_remote_code=True,
+            torch_dtype=torch_dtype
+        )
+
+        tokenizer = transformers.AutoTokenizer.from_pretrained(
+            model_fpath,
+        )
 
     return model, tokenizer
